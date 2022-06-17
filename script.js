5fbd0669b1168e7011ac02b01125f31e68423617
haeriee <97428090+haeriee@users.noreply.github.com>
2022-06-17 14:19:19 +0900

    0617

--- script.js
+++
@@ -1,9 +0,0 @@
-              
-$(function() {
-
-	$(".section .cont").mouseover(function() {					
-		$(".section .cont").stop().animate({width:"153px"}, 900, 'easeOutCirc').removeClass("on");
-		$(this).stop().animate({width:"413px"}, 900, 'easeOutCirc').addClass("on");	
-	});
-
-});
