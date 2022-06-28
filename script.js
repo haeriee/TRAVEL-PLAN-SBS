a258e738ea387f684b9b80ab68b9bfaba62952c0
haeriee <97428090+haeriee@users.noreply.github.com>
2022-06-28 15:01:34 +0900

    0628

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const cols = document.getElementsByClassName('col');
 
-for(let i = 0 ; i<cols.length ; i ++){
+for(let i = 0 ; i < cols.length ; i ++){
     window.addEventListener('scroll', () => {
 
         if(cols[i].getBoundingClientRect().top - window.innerHeight < 0){
